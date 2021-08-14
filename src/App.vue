e85d4e8fc7fdeee40a657bf16aec72a4313ad5ae
ctola91 <ctolapacheco@gmail.com>
2021-08-13 23:49:21 -0400

    router completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <h1>{{ title }}</h1>
+  <div style="display: flex; justify-content: space-evenly">
+    <button @click="goToURL('/')">Go to Home</button>
+
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+    <router-link :to="userURL">Go to User</router-link>
+    <router-link to="/products/test">Go to Product</router-link>
+  </div>
   <router-view></router-view>
 </template>
 <script>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
   name: "App",
   setup() {
     const title = ref("Bienvenidos al curso de Vue.js version 3");
+    const userURL = ref("/users/christian");
+    const router = useRouter();
+
+    const goToURL = (route) => {
+      router.push(route);
+    };
 
     return {
       title,
+      userURL,
+      goToURL,
     };
   },
 };
---
+++ src/views/Product.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Product {{ productId }}</h1>
+</template>
+<script>
+import { useRoute } from "vue-router";
+export default {
+  setup() {
+    const route = useRoute();
+    const productId = route.params.productId;
+
+    return {
+      productId,
+    };
+  },
+};
+</script>
